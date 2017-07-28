7c4e0857c794d9cb0252e60d007e62d208475f11
Dan Tripp <daniel_tripp@yahoo.com>
2017-07-28 14:36:13 -0400

    clean, working - for the parts I have attempted

--- dogs/yearly_rebalance.R
+++ dogs/yearly_rebalance.R
@@ -5,6 +5,10 @@
 # Usage: Rscript yearly_rebalance.R <TotalValueOfDogsToday>
 ##############################################################################################
 
+# TO DO: Get TotalValueOfDogsToday from Ameritrade. Api access is very expensive, but this article says I can use httr:
+#   https://stackoverflow.com/questions/10692066/how-to-webscrape-secured-pages-in-r-https-links-using-readhtmltable-from-xml
+# TO DO: option to run interactively, prompting for input rather than cmd line--though, from what I understand, that is not really what R scripts do.
+
 checkForPackages <- function()
 {
   # Load packages.
@@ -29,37 +33,19 @@ checkForPackages <- function()
 
 main <- function(currentTotalValue)
 {
-  # For now, just prompt for the total value of the current investment.
-  # TO DO: error checking, reprompt, retry option
-  # TO DO: Get this from Ameritrade. Api access is very expensive, but this article says I can use httr:
-  #   https://stackoverflow.com/questions/10692066/how-to-webscrape-secured-pages-in-r-https-links-using-readhtmltable-from-xml
-  readCurrentTotalValue <- function()
-  {
-    currentTotalValue <- readline(prompt = "Enter total value of the dogs today: ")
-    return(currentTotalValue)
-  }
-  # TBD: what does interactive mean, why didn't the script pause after input prompt
-  # TBD: support both prompted input and cmd line params
-  if(interactive())
-  {
-    currentTotalValue <- readCurrentTotalValue()
-  }
-  
   # URL to scrape (current year)
   currentDogsURL <- paste0("http://www.dogsofthedow.com/",strftime(Sys.Date(),"%Y"),"-dogs-of-the-dow.htm")
   
-  # get the page. TO DO: what does useInternalNodes do?
+  # get the page. 
+  # fyi I read that useInternalNodes = TRUE is ignored for HTML docs, but readHTMLTable below fails without it.
   currentDogs.html <- htmlTreeParse(currentDogsURL , useInternalNodes = TRUE)
-  
+
   # parse, just to figure out which table. Leave commented for run time.
   # tables <- readHTMLTable(currentDogs.html,as.data.frame = FALSE)
   
   # get table #12 as a dataframe
   # (skip the row below the header row - it is meta info I do not want/need
   tables <- readHTMLTable(currentDogs.html,skip.rows = 2,stringsAsFactors=FALSE, which = 12)
-  
-  # just for fun. TO DO remove this, I don't need all dow tickers
-  # dowTickers <- tables[1]
   
   # select only the small dogs.
   # spaces in column names are problematic, so get rid of those first
@@ -77,17 +63,15 @@ main <- function(currentTotalValue)
   currentSmallDogsQuotesLast <- currentSmallDogsQuotes$Last
 }
 
-# required input
+# get required input
 args <- commandArgs(TRUE)
 if (length(args) != 1)
 {
   message("Usage: ")
   message("       Rscript yearly_rebalance.R <TotalValueOfDogsToday>")
 } else {
-  #bPackagesFound <- TRUE
+  # confirm dependencies
   goToMain <- checkForPackages()
-  #if (goToMain <- checkForPackages(bPackagesFound))
-  #if (TRUE)
   if(goToMain)
   {
     main(args[1])
